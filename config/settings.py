c7f393d45125d553a4b8f83ce58af8535b658342
Alex Edwards <3017116e@student.gla.ac.uk>
2026-03-11 14:07:16 +0000

    added allowed host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1dupwsh9w%zsep2dimo8h=47_*qb*^o$k1$dj7f(33%-kdexnv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexc6.pythonanywhere.com']
 
 
 # Application definition
